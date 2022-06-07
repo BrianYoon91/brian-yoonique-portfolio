072ac070799f37fc06e964bded255013b10c1ab3
Brian Yoon <yoon.brian91@gmail.com>
2022-06-06 23:11:14 -0400

    nav menu finished with hover effect for each links

--- script/script.js
+++ script/script.js
@@ -1,8 +1,11 @@
 const menuButton = document.querySelector('.hamburger');
 const menuShow = document.querySelector('.menu');
+const changeIcon = document.getElementsByClassName('')
 
-menuButton.addEventListener('click', ()=> {
+menuButton.addEventListener('click', (e) => {
+
     menuShow.classList.toggle('active');
     menuButton.classList.toggle('active');
-
+    e.target.classList.toggle('fa-square-xmark');
+    e.target.classList.toggle('fa-bars');
 })
